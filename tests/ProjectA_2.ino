a6a4db969d29ca82f61618b1421828af18a44185
khnishou <kerdelyounes@gmail.com>
2026-04-21 14:27:31 +0200

    create project for 2nd part project A

---
+++ tests/ProjectA_2.ino
@@ -0,0 +1,149 @@
+int left10 = 10;
+int right11 = 11;
+
+int pingPin = 7;
+float v = 331.5+0.6*20;
+
+int headPin = 12;
+
+volatile int count_r = 0;
+volatile int count_l = 0;
+
+const float distParImpulsion = 0.325;
+
+void roue_r()
+{
+  count_r++;
+}
+
+void roue_l()
+{
+  count_l++;
+}
+
+void setup()
+{
+  pinMode(pingPin, OUTPUT);
+  pinMode(left10, OUTPUT);
+  pinMode(right11, OUTPUT);
+
+  Serial.begin(9600);
+
+  attachInterrupt(digitalPinToInterrupt(2), roue_r, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(3), roue_l, CHANGE);
+
+  randomSeed(analogRead(A0));
+}
+
+void loop()
+{
+  roulerVerif_isSpace(1);
+  if (roulerVerif_dist(17))
+  {
+    // creneau
+  }
+}
+
+int isSpace()
+{
+  return (distanceCm() > 10);
+}
+
+void pulseServo(int servoPin, int pulseLg)
+{
+  digitalWrite(servoPin, HIGH);
+  delayMicroseconds(pulseLg);
+  digitalWrite(servoPin, LOW);
+}
+
+int roulerVerif_dist(float distanceCm)
+{
+  count_r = 0;
+  count_l = 0;
+
+  int cible = abs(distanceCm) / distParImpulsion;
+
+  while (count_l < cible && count_r < cible)
+  {
+    if (!isSpace())
+      return (0);
+    if (count_l < cible)
+      pulseServo(left10, (distanceCm > 0) ? 1700 : 1300);
+
+    if (count_r < cible)
+      pulseServo(right11, (distanceCm > 0) ? 1300 : 1700);
+
+    delay(20);
+  }
+  return 1;
+}
+
+void roulerVerif_isSpace(int direc)
+{
+  count_r = 0;
+  count_l = 0;
+
+  while (1)
+  {
+    if (isSpace())
+      break ;
+    if (count_l < count_r)
+      pulseServo(left10, (direc > 0) ? 1700 : 1300);
+
+    if (count_r < count_l)
+      pulseServo(right11, (direc > 0) ? 1300 : 1700);
+
+    delay(20);
+  }
+}
+
+// void tournerSurLuiPrecis(int angle)
+// {
+//   count_r = 0;
+//   count_l = 0;
+
+//   int cible = (abs(angle) * 33.24) / (360 * distParImpulsion);
+
+//   int varL = (angle > 0) ? 1700 : 1300;
+//   int varR = (angle > 0) ? 1700 : 1300;
+
+//   while (count_l < cible && count_r < cible) {
+//     if (count_l < cible)
+//       pulseServo(left10, varL);
+
+//     if (count_r < cible)
+//       pulseServo(right11, varR);
+
+//     delay(20);
+//   }
+// }
+
+float microsecondsToCentimeters(float microseconds) {
+  return microseconds / 58;
+}
+
+float distanceCm()
+{
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+  pinMode(pingPin, INPUT);
+  float duration = pulseIn(pingPin, HIGH);
+  float t = duration /29.0/2 ;
+  float d = v*t ;
+
+  return microsecondsToCentimeters(duration); 
+} 
+
+void tournerMoteur(float d)
+{
+  if (d > 17)
+    roulerPrecis(1);
+  else if (d < 13)
+    roulerPrecis(-1);
+  else
+    roulerPrecis(0);
+}
